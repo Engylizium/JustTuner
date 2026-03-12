7879e2c41efa08539d6a8f9407b96fc6d5361bba
Engylizium <22287855+Engylizium@users.noreply.github.com>
2026-03-12 20:31:08 +0400

    Initial Commit

---
+++ JustTuner/JustTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustTunerApp.swift
+//  JustTuner
+//
+//  Created by Engylizium on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct JustTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
